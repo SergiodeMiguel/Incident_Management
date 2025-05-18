72ab7971782bfe80670703935a993dde87b1a543
Sergio De Miguel <sergiodm2424@gmail.com>
2025-05-19 01:03:56 +0200

    db connection file comment update

--- SERVER/Includes/db_connection.php
+++ SERVER/Includes/db_connection.php
@@ -1,10 +1,10 @@
 <?php
 /*
+     ---------- DATABASE CONNECTION FILE ----------
 I have made the database connection configuration in a separate file so that I don't have to repeat the connection manually in each one.
 I avoid duplication of code and it is easy to maintain, since the configuration is only changed here.
 */
 
-// ---------- DATABASE CONNECTION ----------
 
 // Define database connection credentials
 $host = 'localhost';
